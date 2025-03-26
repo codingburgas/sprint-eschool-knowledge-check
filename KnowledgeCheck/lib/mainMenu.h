3adabe0d26dcead2b73a81ae12089476b2f9a730
ADKokalarov23 <ADKokalarov23@codingburgas.bg>
2025-03-26 20:22:33 +0200

    Made biology test

--- KnowledgeCheck/lib/mainMenu.h
+++ KnowledgeCheck/lib/mainMenu.h
@@ -1,13 +1,1 @@
 #pragma once
-#include "../lib/precompile.h"
-
-class MainMenu {
-public:
-	MainMenu();
-	Texture background;
-	Texture testsButton;
-	Texture learnButton;
-	Texture title;
-	Rectangle testsButtonRec;
-	void loadResources();
-};
